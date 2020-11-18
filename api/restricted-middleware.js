be6d53c546d916ace61d12d30861560c5c1e1b21
hussainabutt <68175777+hussainabutt@users.noreply.github.com>
2020-11-18 17:02:25 -0500

    test

--- api/restricted-middleware.js
+++ api/restricted-middleware.js
@@ -4,7 +4,7 @@ module.exports = ( req, res, next) => {
     const token = req.headers.authorization;
 
     if(token){
-        jwt.verify(token, process.env.secret, (err, decodedToken) => {
+        jwt.verify(token, process.env.SECRET, (err, decodedToken) => {
             if (err) {
                 res.status(401).json({message: 'Not Authorized'})
             } else {
