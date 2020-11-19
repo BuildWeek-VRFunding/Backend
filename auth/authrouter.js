74335875ef0e97fc6da7a340d833f1e73711a335
hussainabutt <68175777+hussainabutt@users.noreply.github.com>
2020-11-19 18:24:56 -0500

    All crud operations implemented

--- auth/authrouter.js
+++ auth/authrouter.js
@@ -2,6 +2,72 @@ const router = require('express').Router();
 
 const Users = require("../database/users-model")
 const fund = require('../database/auth-model')
+//CREATE FUNDRAISER
+router.post('/fundraiseradd', (req, res) => {
+    const { id } = req.params;
+    const recipe = req.body;
+    console.log(recipe);
+ 
+    Recipes.add(recipe)
+       .then(data => {
+          res.status(201).json(data);
+       })
+       .catch(err => {
+          console.log(err)
+          res.status(500).json({ message: 'fail to create new fundraiser' })
+       })
+ })
+//CREATE DONATION
+router.post('/donationadd/:id', (req, res) => {
+    const donation = req.body;
+    console.log(donation);
+ 
+    fund.adddonation(donation)
+       .then(data => {
+          res.status(201).json(data);
+       })
+       .catch(err => {
+          console.log(err)
+          res.status(500).json({ message: 'fail to add donation' })
+       })
+ })
+//PUT FUNDRAISER BY ID 
+router.put('/fundraiseredit/:id', (req, res)=> {
+    const id =req.params.id;
+    const changes = req.body;
+    fund.update(id, changes)
+        .then(data =>{
+            res.json(data)
+        })
+        .catch(err => {
+            res.status(404).json({message: 'could not update fundraiser', err})
+        })
+
+})
+
+//DELETE FUNDRAISER BY ID
+router.delete('./fundraiserdel/:id', (req,res) => {
+    const id = req.params.id;
+    fund.remove(id)
+    .then(data =>{
+        res.json(data)
+    })
+    .catch(err => {
+        res.status(404).json({message: 'could not delete fundraiser', err})
+    })
+
+})
+router.get('/fundraiserall/:id', (req, res) => {
+    const id = req.params.id;
+    fund.getfundraiser(id)
+        .then(data => {
+            res.json(data)
+        })
+        .catch(err => {
+            res.status(404).json({message: 'could not find fundraiser', err})
+        })
+})
+
 //GET FUNDRAISER BY ID
 router.get('/fundraiser/:id', (req, res) => {
     const id = req.params.id;
--- database/auth-model.js
+++ database/auth-model.js
@@ -1,11 +1,46 @@
 const db = require("./dbconfig");
 
 module.exports = {
+    addfund,
+    adddonation,
     findDonationByID,
     findById,
-    getDonationTotalByID
+    getDonationTotalByID,
+    getfundraiser,
+    update,
+    remove
 }
-
+function addfund(data) {
+    return db('fundraiser')
+       .insert(data, 'id')
+       .then(ids => {
+          return getfundraiser(ids[0]);
+       })
+    }
+       function adddonation(data) {
+        return db('donation')
+           .insert(data)
+           .then(ids => {
+              return findDonationByID(ids[0]);
+           })
+        }
+function update(id, changes) {
+    return db('fundraiser').where({ id }).update(changes)
+       .then(data => {
+          return findById(id)
+       })
+ }
+ 
+ function remove(id) {
+    return db('fundraiser')
+       .where({ id })
+       .del()
+ }
+function getfundraiser(id){
+    return db("fundraiser as f").join("donations as d", "d.funderid", "f.ownerid")
+            .where("f.ownerid", "=", id)
+        
+}
 function findById(id){
     return db("fundraiser").where("ownerid", "=", id);
 }
